f1dec9d6dcbe37c1d729021659c29bf5e2a11215
chunny <shdsdh@naver.com>
2026-04-27 18:46:37 +0900

    Title : 5주차 과제_춘삼

---
+++ Umc10thChunSam/src/main/java/com/example/umc10thchunsam/domain/member/dto/res/MemberMissionResDto.java
@@ -0,0 +1,13 @@
+package com.example.umc10thchunsam.domain.member.dto.res;
+
+import com.example.chunsam.domain.mission.enums.MissionStatus;
+
+import java.time.LocalDate;
+
+public class MemberMissionResDto {
+    private Long mission_id;
+    private Long member_id;
+    private MissionStatus issuccess;   // BEFORE / IN_PROGRESS / COMPLETED
+    private LocalDate successDate;
+
+}
