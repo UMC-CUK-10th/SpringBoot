189c7a2a92811101b5c70027f299ade6aa9cfaff
chunny <shdsdh@naver.com>
2026-04-27 19:03:19 +0900

    Title : 5주차 과제_춘삼

--- Umc10thChunSam/src/main/java/com/example/umc10thchunsam/domain/member/exception/MemberException.java
+++ Umc10thChunSam/src/main/java/com/example/umc10thchunsam/domain/member/exception/MemberException.java
@@ -9,3 +9,6 @@ public class MemberException extends GeneralException {
     }
 
 }
+//실수로 로컬에서 브랜치 새로 안파고 기존 브랜치에 push했더니 PR을 생성할 수 가 없네여
+//그래서 로컬에서 새로 브랜치 파고 다시 올려요. 새롭게 추가 된 코드들은 기존 4주차 2번쨰 commit에서 확인 할 수 있습니다
+//5주차 pr에서 찾아봐도 되는데 번거로와서 ㅋ;;
